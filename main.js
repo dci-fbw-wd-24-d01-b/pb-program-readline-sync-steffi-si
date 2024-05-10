6c3a4bd1873c96dd000d3a8d4ba3f697b4009f36
Stefanie <multa.excogitare@gmail.com>
2024-05-10 10:15:20 +0200

    inital commit

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import readlineSync from "readline-sync";
+
+const name = readlineSync.question("What is your name? ");
+const age = parseInt(readlineSync.question("In which year were you born? "));
+const birthPlace = readlineSync.question("In which city were you born? ");
+const actualCity = readlineSync.question("Were do you live now? ");
+const currentYear= parseInt(readlineSync.question("What is the current year? "));
+const currentAge = currentYear - age;
+
+console.log('Hello! My name is ' + name + '. I was born ' + currentAge + ' years ago in ' + birthPlace + '. Now I live in ' + actualCity + '.');
